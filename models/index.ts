a78e8f149122091e28dc6a09541e809a51e9af51
Luke Wilkinson <6750242+Lexical-Luke@users.noreply.github.com>
2023-01-27 14:30:47 +0200

    Added openapi-generator-cli

---
+++ models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Detection';
+export * from './Frame';
+export * from './FrameDetection';
+export * from './HTTPValidationError';
+export * from './LocationInner';
+export * from './ValidationError';
+export * from './Video';
